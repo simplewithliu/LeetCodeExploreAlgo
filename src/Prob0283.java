0320c1148b1cd9e818e4497eee43bb0c0fdf2c25
sorrowfulliu <3185435464@qq.com>
2020-09-13 22:36:55 +0800

    Sept 13 2020

---
+++ src/Prob0283.java
@@ -0,0 +1,43 @@
+package com.leetcode.explore;
+
+/*
+
+移动零
+
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+
+说明:
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+
+*/
+
+public class Prob0283
+{
+	class Solution
+	{
+		public void moveZeroes(int[] nums)
+		{
+			if (nums.length == 0)
+			{
+				return;
+			}
+			for (int last = 0, cur = 0; cur < nums.length; ++cur)
+			{
+				if (nums[cur] != 0)
+					swap(nums, last++, cur);
+			}
+		}
+
+		private void swap(int[] nums, int i, int j)
+		{
+			int temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
+		}
+	}
+}
