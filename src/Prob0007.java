4ce0b686c86e5f3bb7be73c39ea0dbeb325784b2
sorrowfulliu <3185435464@qq.com>
2020-10-11 20:44:15 +0800

    Oct 11 2020

---
+++ src/Prob0007.java
@@ -0,0 +1,47 @@
+package com.leetcode.explore;
+
+/*
+
+整数反转
+
+给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+示例 1:
+输入: 123
+输出: 321
+
+示例 2:
+输入: -123
+输出: -321
+
+示例 3:
+输入: 120
+输出: 21
+
+注意:
+假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+
+*/
+
+public class Prob0007 {
+	class Solution {
+		public int reverse(int x) {
+			boolean sign = x < 0 ? true : false;
+			int res = 0;
+			while (x != 0) {
+				if (sign) {
+					if ((Integer.MIN_VALUE / 10 > res) || (Integer.MIN_VALUE / 10 == res && Integer.MIN_VALUE % 10 > x)) {
+						return 0;
+					}
+				} else {
+					if ((Integer.MAX_VALUE / 10 < res) || (Integer.MAX_VALUE / 10 == res && Integer.MAX_VALUE % 10 < x)) {
+						return 0;
+					}
+				}
+				res = res * 10 + x % 10;
+				x /= 10;
+			}
+			return res;
+		}
+	}
+}
---
+++ src/Prob0125.java
@@ -0,0 +1,41 @@
+package com.leetcode.explore;
+
+/*
+
+验证回文串
+
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+输入: "A man, a plan, a canal: Panama"
+输出: true
+
+示例 2:
+输入: "race a car"
+输出: false
+
+*/
+
+public class Prob0125 {
+	class Solution {
+		public boolean isPalindrome(String s) {
+			if (s.isEmpty()) {
+				return true;
+			}
+			for (int i = 0, j = s.length() - 1; i < j; ++i, --j) {
+				while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+					++i;
+				}
+				while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+					--j;
+				}
+				if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
