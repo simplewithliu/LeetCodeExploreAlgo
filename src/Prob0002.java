a04245f010efd3bbe36be84aef4e0f5918ae801a
sorrowfulliu <3185435464@qq.com>
2021-08-21 14:11:14 +0800

    Aug 21 2021

---
+++ src/Prob0002.java
@@ -0,0 +1,57 @@
+package com.leetcode.explore;
+
+/*
+
+2. 两数相加
+
+给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例 1：
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807
+
+示例 2：
+输入：l1 = [0], l2 = [0]
+输出：[0]
+
+示例 3：
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+
+提示：
+每个链表中的节点数在范围 [1, 100] 内
+0 <= Node.val <= 9
+题目数据保证列表表示的数字不含前导零
+
+*/
+
+public class Prob0002 {
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(-1);
+            ListNode node = dummy;
+            int sum = 0;
+            while (l1 != null || l2 != null) {
+                sum /= 10;
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                node.next = new ListNode(sum % 10);
+                node = node.next;
+            }
+            if (sum / 10 != 0) {
+                node.next = new ListNode(1);
+            }
+            ListNode head = dummy.next;
+            return head;
+        }
+    }
+}
