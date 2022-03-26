260e59264ab5dc0b662617fa848b6e4886ef5baa
sorrowfulliu <3185435464@qq.com>
2022-03-27 01:03:47 +0800

    Mar 27 2022

--- src/Prob0117.java
+++ src/Prob0117.java
@@ -33,6 +33,28 @@ struct Node {
 */
 
 public class Prob0117 {
+
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
     // µü´ú
     class Solution1 {
         public Node connect(Node root) {
@@ -95,23 +117,3 @@ public class Prob0117 {
     }
 }
 
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {
-    }
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
